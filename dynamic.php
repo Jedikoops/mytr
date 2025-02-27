b7a0bb6fd97419ed8f9cd1dfa7377312e103545c
Justin Koops <43502813+Jedikoops@users.noreply.github.com>
2025-02-27 01:31:07 -0500

    testtwo

---
+++ dynamic.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="style3.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="bucket.html">Bucketuber</a></li>
+            <li><a href="hacked.html">Hacked</a></li>
+            <li><a href="dynamic.php">Dynmaic</a></li>
+        </ul>
+
+        <?php
+        echo "meow";
+        ?>
+
+        <!--this file is currently broken because PHP cannot be parsed by godaddy -->
+
+    </body>
+</html>
